9e143cdc29d78447ed7412e359bdfb6f4adaefa6
gustavo-dominguez-9 <gustavo.dominguez@iest.edu.mx>
2026-02-20 08:24:36 -0600

    corrección

---
+++ PROYECTO04-GUSTAVO/PROYECTO04-GUSTAVO/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Numero 1 As Integer = Integer.parse(TextBox2.text)
+        Dim Numero 2 As Integer = Integer.parse(TextBox1.text)
+        Dim resultado As Integer = Numero1 + Numero2
+        Label4.Text = resultado.ToString()
+    End Sub
+End Class
